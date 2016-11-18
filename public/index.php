2499d9dca2b52707d1b0f04d102ddfab56130028
Paul Pröll <paul.proell@hotmail.com>
2016-11-18 12:17:46 +0100

    installed slim

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->get('/test', function (Request $request, Response $response) {
+    $response->getBody()->write("test");
+
+    return $response;
+});
+$app->run();
